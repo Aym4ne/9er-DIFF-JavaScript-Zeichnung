6939bfff44b2e43c148d14bd792f5a8f52f784d4
Jean-Pierre <jean.ambrosius@tu-dortmund.de>
2025-06-18 08:34:20 +0200

    Initial Commit. Beispielprojekt, bei dem ein roter Kreis und ein blaues Rechteck gezeichnet werden.

---
+++ src/script.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    const canvas = document.getElementById("meinCanvas"); //Das <canvas>-Element bietet eine Zeichenfläche.
+    const ctx = canvas.getContext("2d"); //Im JavaScript wird das 2D-Zeichenkontextobjekt (ctx) verwendet, um Grafiken zu zeichnen.
+
+    // Roter ausgefüllter Kreis
+    ctx.beginPath();
+    ctx.arc(100, 100, 50, 0, Math.PI * 2); // x, y, radius, startAngle, endAngle
+    ctx.fillStyle = "red";
+    ctx.fill();
+
+    // Blaues nicht ausgefülltes Rechteck
+    ctx.beginPath();
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 3;
+    ctx.rect(200, 70, 120, 80); // x, y, width, height
+    ctx.stroke();
+};
